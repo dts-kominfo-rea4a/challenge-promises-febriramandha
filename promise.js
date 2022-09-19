41994a42e5f9714571561befb8c94794328868aa
febriramandha <febri.ramandha@gmail.com>
2022-09-19 20:55:43 +0700

    Tugas Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,24 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
-
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (ekspresi) => {
+  let hasilNonton = 0;
+  try {
+    const TheaterIXX = await promiseTheaterIXX();
+    const TheaterVGC = await promiseTheaterVGC();
+    const result = [...TheaterIXX, ...TheaterVGC];
+    
+    result.forEach((Element) => {
+      if (Element.hasil == ekspresi) {
+        hasilNonton++;
+      }
+    });
+    if (hasilNonton > 0) {
+      return hasilNonton;
+    };
+  } catch (error) {
+    console.log(error)
+  }
+};
 
 module.exports = {
   promiseOutput,
